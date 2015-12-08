1d9ce4801c7fda24f66670bd49bd8c7c2dc37b6f
Daniel Kats <dbkats@cs.toronto.edu>
2015-12-07 21:47:42 -0500

    delete button UI is better

--- react_components/passzero_react.jsx
+++ react_components/passzero_react.jsx
@@ -35,16 +35,18 @@ var Entry = React.createClass({
                 <span className="back-button glyphicon glyphicon-chevron-left" role="button"
                     onClick={ this.props.onBack }></span>
                 <div className="entry">
-                    <div className="entry-account">{ this.props.entry.account }</div>
+                    <div className="entry-account">
+                        <span>{ this.props.entry.account }</span>
+                        <span id="entry-delete-btn" role="button"
+                            className="glyphicon glyphicon-remove"
+                            onClick={ this.props.onDeleteClick }></span>
+                    </div>
                     <div className="entry-username">{ this.props.entry.username }</div>
                     <div className="entry-password password-hidden"
                     onClick={ this.handlePasswordClick }>
                         { this.props.entry.password }
                     </div>
                 </div>
-                <button type="button"
-                className="btn btn-danger"
-                onClick={ this.props.onDeleteClick }>Delete</button>
             </div>
         );
     }
